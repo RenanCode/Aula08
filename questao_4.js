f0ed1cffe0ba63f288bf627304ed966813c0e21e
Dev ifsp ptb <dev.ifsp.ptb@gmail.com>
2019-10-09 21:38:25 -0300

    Update

--- questao_4.js
+++ questao_4.js
@@ -1,19 +1,42 @@
-class Aluno{
-    constructor(nome_aluno,n1,n2){
-        this.nome_aluno = nome_aluno;
-        this.n1 = n2;
-        this.n2 = n2;
+class Aluno {
+        constructor(primeiroNome, segundoNome, primeiraNota, segundaNota) {
+            this.primeiroNome = primeiroNome;
+            this.segundoNome = segundoNome;
+            this.primeiraNota = primeiraNota;
+            this.segundaNota = segundaNota;
+        }
+
+        nomeAluno(primeiroNome, segundoNome) {
+            return nomeCompleto = primeiroNome + segundoNome;
+        }
+
+        media = mediaAluno(primeiraNota, segundaNota){
+        return media = (primeiraNota * 0.6) + (segundaNota * 0, 4);
     }
 
-    multiplicacao(n1,n2) {
-        a = this.n1*0.6;
-        b = this.n2*0.4;
-        c = a + b;
-        return a,b,c;
-      }
+    situacaoAluno(){
+        if (media > 6) {
+            alert("Aluno aprovado !");
+        }
+        else {
+            alert("Aluno reprovado !");
+        }
+    }
 
 }
-var aluno = new Aluno("João da Silva",7,8.5);
-var v1 = multiplicacao(aluno.n1,aluno.n2);
 
-alert("O nome do aluno é: ");
+var al_1 = new Aluno("Renan", "Costa", 10, 8);
+var al_2 = new Aluno("Gustavo", "Peleje", 5, 5);
+var al_3 = new Aluno("Jader", "Thome", 8, 8);
+var al_4 = new Aluno("Rafael", "Serra", 7, 7);
+var al_5 = new Aluno("Julio", "Henrique", 5, 5);
+
+var alunos = new Array();
+var alunos = [al_1, al_2, al_3, al_4, al_5];
+document.getElementById("demo").innerHTML = alunos;
+
+
+
+var media_global = media();
+
+
